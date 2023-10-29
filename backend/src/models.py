fdaffe0de117843564d85416ac720e7b4921ca51
Albin Lindqvist <albin.lindqvist@hotmail.se>
2023-10-29 15:51:17 +0000

    Create user table

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,2 +1,8 @@
 """All models for the project are defined here."""
+from sqlalchemy.orm import mapped_column, Mapped
+
 from src.database import Base
+
+class User(Base):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column()
