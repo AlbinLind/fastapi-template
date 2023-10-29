8efdd4614c0c23c5c131a27150b706650d693cd3
Albin Lindqvist <albin.lindqvist@hotmail.se>
2023-10-29 15:56:11 +0000

    Add offset and limit to get_all_user query

--- backend/src/user/router.py
+++ backend/src/user/router.py
@@ -1,3 +1,4 @@
+"""Api endpoints for the User object."""
 from fastapi import APIRouter
 from sqlalchemy import select, text
 
@@ -10,19 +11,21 @@ router = APIRouter()
 
 @router.get("/")
 def get_all_user(limit: int = 30, offset: int = 30) -> list[UserBase]:
-    query = select(User)
-    query_result = database.fetch_all(query)
-    return query_result
+    """Get all users that are in the database."""
+    query = select(User).offset(offset).limit(limit)
+    return database.fetch_all(query)
 
 
 @router.get("/{user_id}")
 def get_user_by_id(user_id: int) -> UserBase:
+    """Get a specific user."""
     query = select(User).where(User.id == user_id)
     return database.fetch_one(query)
 
 
 @router.post("/")
 def add_user(user: UserCreate) -> dict[str, str]:
+    """Create a user."""
     user = User(**user.model_dump())
     database.add(user)
     return {"message": "User created"}
